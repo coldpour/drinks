4e573ae02616c70c236da0a64f12ef765275d0ee
Mike Holm <mholm@rallydev.com>
2018-04-01 12:38:32 -0600

    add presets

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 import Result as Result
 
 
@@ -68,36 +68,89 @@ view model =
     div []
         [ h1 [] [ text "Drinks!" ]
         , p [] [ text "How many drinks is that high percentage beer?" ]
-        , label [ class "percent" ]
-            [ text "percent"
-            , input
-                [ type_ "number"
-                , value model.percent
-                , onInput UpdatePercent
-                ]
-                []
+        , percentView model.percent
+        , volumeView model.volume
+        , drinksView
+            (calculateDrinks
+                (Result.withDefault 1 (String.toFloat model.percent))
+                (Result.withDefault 0 (String.toFloat model.volume))
+            )
+        ]
+
+
+percentView : String -> Html Msg
+percentView percent =
+    label [ class "percent" ]
+        [ text "percent"
+        , input
+            [ type_ "number"
+            , value percent
+            , onInput UpdatePercent
             ]
-        , label [ class "volume" ]
+            []
+        ]
+
+
+volumeView : String -> Html Msg
+volumeView volume =
+    div [ class "volume-view" ]
+        [ label [ class "volume" ]
             [ text "volume"
             , input
                 [ type_ "number"
-                , value model.volume
+                , value volume
                 , onInput UpdateVolume
                 ]
                 []
             ]
-        , div [ class "drinks" ]
-            [ text
-                (String.append
-                    "drinks: "
-                    (toString
-                        (calculateDrinks
-                            (Result.withDefault 0 (String.toFloat model.percent))
-                            (Result.withDefault 0 (String.toFloat model.volume))
-                        )
-                    )
-                )
-            ]
+        , volumePresetsView
+        ]
+
+
+volumePresetsView : Html Msg
+volumePresetsView =
+    div [ class "volume-presets" ]
+        (List.map buttonPreset volumePresets)
+
+
+volumePresets : List Preset
+volumePresets =
+    List.map presetFromTuple
+        [ ( "tulip", 10 )
+        , ( "can", 12 )
+        , ( "pint", 16 )
+        , ( "bomber", 22 )
+        ]
+
+
+presetFromTuple : ( String, Float ) -> Preset
+presetFromTuple tuple =
+    (Preset (Tuple.first tuple) (Tuple.second tuple))
+
+
+type alias Preset =
+    { name : String
+    , value : Float
+    }
+
+
+buttonPreset : Preset -> Html Msg
+buttonPreset { name, value } =
+    button
+        [ class name
+        , onClick (UpdateVolume (toString value))
+        ]
+        [ text name ]
+
+
+drinksView : Float -> Html Msg
+drinksView drinks =
+    div [ class "drinks" ]
+        [ text
+            (String.append
+                "drinks: "
+                (toString drinks)
+            )
         ]
 
 
@@ -109,12 +162,3 @@ view model =
 calculateDrinks : Float -> Float -> Float
 calculateDrinks percent volume =
     (percent * volume) / (40 * (3 / 2))
-
-
-
--- onInput : (Float -> msg) -> Attribute msg
--- onInput tagger =
---     on "input" (Json.map tagger targetValue)
--- targetValue : Json.Decoder Float
--- targetValue =
---     Json.at [ "target", "value" ] Json.float
